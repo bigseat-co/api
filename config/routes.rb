3bae2f2f12dad2406cf0e85d030dd5273c9cc450
Jeremie Ges <ges.jeremie@gmail.com>
2021-01-17 03:47:16 -0800

    chore: update database.yml, drop sidekiq integration for the time being

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq' # Protected by basic_auth (ie. config/initializers/sidekiq.rb)
+  #mount Sidekiq::Web => '/sidekiq' # Protected by basic_auth (ie. config/initializers/sidekiq.rb)
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Room.create!(name: 'My very first room!')
+#Room.create!(name: 'My very first room!')
