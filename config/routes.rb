bc05aeba8dddf7cda11613a74a345770e063b938
Jeremie Ges <ges.jeremie@gmail.com>
2021-01-17 02:41:52 -0800

    chore: setup docker-compose, redis, postgresql, sidekiq, sidekiq-cron, sidekiq web ui, revamp README

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' # Protected by basic_auth (ie. config/initializers/sidekiq.rb)
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
