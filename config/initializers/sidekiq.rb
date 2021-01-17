bd770ea8598dbf433112434097e4ae420af2d5e6
Jeremie Ges <ges.jeremie@gmail.com>
2021-01-17 07:17:19 -0800

    chore: fix sidekiq bug

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,5 @@
 require 'sidekiq'
 require 'sidekiq/web'
-require 'sidekiq/testing'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
@@ -15,5 +14,6 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
 end
 
 if Rails.env.development?
+  require 'sidekiq/testing'
   Sidekiq::Testing.inline!
 end
