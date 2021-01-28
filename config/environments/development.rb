e6c9fd08142c15a8e75ca73e5a581947c71d331e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-01-28 23:56:17 +0100

    chore: config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Let anyone call the API
+  config.hosts.clear
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
