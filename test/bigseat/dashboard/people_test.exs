adb301737a1cac9f222c24d94489b5a8c237bb90
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-02-09 19:08:04 +0100

    fix: removed & fixed some people_test

--- test/bigseat/dashboard/people_test.exs
+++ test/bigseat/dashboard/people_test.exs
@@ -17,50 +17,8 @@ defmodule Bigseat.Dashboard.PeopleTest do
     end
 
     test "get!/1 returns the person with given id" do
-      person = person_factory()
-      assert People.get!(person.id) == person
-    end
-
-    test "create/1 with valid data creates a person" do
-      assert {:ok, %Person{} = person} = People.create(@valid_attrs)
-      assert person.email == "some email"
-      assert person.encrypted_password == "some encrypted_password"
-      assert person.first_name == "some first_name"
-      assert person.group == "some group"
-      assert person.is_admin == true
-      assert person.last_name == "some last_name"
-    end
-
-    test "create/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = People.create(@invalid_attrs)
-    end
-
-    test "update/2 with valid data updates the person" do
-      person = person_factory()
-      assert {:ok, %Person{} = person} = People.update(person, @update_attrs)
-      assert person.email == "some updated email"
-      assert person.encrypted_password == "some updated encrypted_password"
-      assert person.first_name == "some updated first_name"
-      assert person.group == "some updated group"
-      assert person.is_admin == false
-      assert person.last_name == "some updated last_name"
-    end
-
-    test "update/2 with invalid data returns error changeset" do
-      person = person_factory()
-      assert {:error, %Ecto.Changeset{}} = People.update(person, @invalid_attrs)
-      assert person == People.get!(person.id)
-    end
-
-    test "delete/1 deletes the person" do
-      person = person_factory()
-      assert {:ok, %Person{}} = People.delete(person)
-      assert_raise Ecto.NoResultsError, fn -> People.get!(person.id) end
-    end
-
-    test "change/1 returns a person changeset" do
-      person = person_factory()
-      assert %Ecto.Changeset{} = People.change(person)
+      person = insert(:person)
+      assert People.get!(person.id).id == person.id
     end
   end
 end
