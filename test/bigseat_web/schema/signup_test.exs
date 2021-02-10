1b836d9f5ee1f0e7f3bafe5f302e68e9f9a5091e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-02-10 21:32:42 +0100

    test: with slug added

--- test/bigseat_web/schema/signup_test.exs
+++ test/bigseat_web/schema/signup_test.exs
@@ -1,7 +1,10 @@
 defmodule BigseatWeb.Schema.SignupTest do
   use BigseatWeb.ConnCase, async: true
   use Bigseat.HelpersCase
-  alias Bigseat.Dashboard.Person
+  alias Bigseat.Dashboard.{
+    Person,
+    Organization
+  }
 
   describe "spaces" do
     setup do
@@ -17,7 +20,30 @@ defmodule BigseatWeb.Schema.SignupTest do
     end
 
     test "signup a new person", %{conn: conn, signup: signup} do
-      mutation = """
+      mutation = signup |> valid_mutation
+      response = conn |> graphql_query(mutation, :success)
+
+      assert response == %{"data" => %{"signup" => %{"id" => first_person().id}}}
+    end
+
+    test "signup a new person with a specific organization slug", %{conn: conn, signup: signup} do
+      mutation = signup |> valid_mutation_with_slug("valid-slug")
+      response = conn |> graphql_query(mutation, :success)
+
+      assert response == %{"data" => %{"signup" => %{"id" => first_person().id}}}
+      assert first_organization().slug == "valid-slug"
+    end
+
+    defp first_person do
+      Person |> first() |> Repo.one()
+    end
+
+    defp first_organization do
+      Organization |> first() |> Repo.one()
+    end
+
+    defp valid_mutation(signup) do
+      """
       mutation {
         signup(
           email: "#{signup.email}"
@@ -32,10 +58,25 @@ defmodule BigseatWeb.Schema.SignupTest do
         }
       }
       """
+    end
 
-      response = graphql_query(conn, mutation, :success)
-      created_person = Person |> first() |> Repo.one()
-      assert response == %{"data" => %{"signup" => %{"id" => created_person.id}}}
+    defp valid_mutation_with_slug(signup, slug) do
+      """
+      mutation {
+        signup(
+          email: "#{signup.email}"
+          firstName: "#{signup.first_name}"
+          lastName: "#{signup.last_name}"
+          organization: {
+            name: "#{signup.organization.name}"
+            slug: "#{slug}"
+          }
+          password: "#{signup.password}"
+        ) {
+          id
+        }
+      }
+      """
     end
   end
 end
