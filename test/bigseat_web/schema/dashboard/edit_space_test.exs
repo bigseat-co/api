d18efc4da2705cab890a5e9752875eb099d1a7e2
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-02-21 20:59:33 +0100

    test: edit space

--- test/bigseat_web/schema/dashboard/edit_space_test.exs
+++ test/bigseat_web/schema/dashboard/edit_space_test.exs
@@ -1,47 +1,65 @@
 defmodule BigseatWeb.Schema.EditSpaceTest do
-  # use BigseatWeb.ConnCase, async: true
-  # alias Bigseat.Factory.{
-  #   PersonFactory,
-  #   SpaceFactory
-  # }
-  # use Bigseat.HelpersCase
+  use BigseatWeb.ConnCase, async: true
+  alias Bigseat.Factory.{
+    PersonFactory,
+    SpaceFactory
+  }
+  use Bigseat.HelpersCase
 
-  # describe "get_space" do
-  #   setup do
-  #     [
-  #       space: SpaceFactory.insert(:space),
-  #       person: PersonFactory.insert(:person, is_admin: true)
-  #     ]
-  #   end
+  describe "get_space" do
+    setup do
+      person = PersonFactory.insert(:person, is_admin: true)
 
-  #   test "without authentication", %{conn: conn, space: space} do
-  #     response = graphql_query(conn, %{query: query(), variables: space |> variables()}, :success)
-  #     assert Map.has_key?(response, "errors")
-  #   end
+      [
+        space: SpaceFactory.insert(:space, organization: person.organization),
+        person: person,
+        person_from_another_organization: PersonFactory.insert(:person, is_admin: true)
+      ]
+    end
 
-  #   test "by id", %{conn: conn, space: space, person: person} do
-  #     auth_conn = conn |> authorize(person)
+    test "without authentication", %{conn: conn, space: space} do
+      space_input = %{name: "Random name"}
+      response = graphql_query(conn, %{query: query(), variables: space |> variables(%{name: "Random name"})}, :success)
+      assert Map.has_key?(response, "errors")
+    end
 
-  #     response = graphql_query(auth_conn, %{query: query(), variables: space |> variables()}, :success)
-  #     assert response == %{"data" => %{"getSpace" => %{"id" => "#{space.id}"}}}
-  #   end
+    test "with authentication", %{conn: conn, space: space, person: person} do
+      auth_conn = conn |> authorize(person)
 
-  #   defp query() do
-  #     """
-  #     query(
-  #       $id: UUID4!
-  #     ) {
-  #       getSpace(id: $id) {
-  #         id
-  #       }
-  #     }
-  #     """
-  #   end
+      response = graphql_query(auth_conn, %{query: query(), variables: space |> variables(%{name: "Random name"})}, :success)
+      assert response == %{"data" => %{"editSpace" => %{"id" => "#{space.id}", "name" => "Random name"}}}
+    end
 
-  #   def variables(space) do
-  #     %{
-  #       id: space.id
-  #     }
-  #   end
-  # end
+    test "with authentication from another organization", %{conn: conn, space: space, person_from_another_organization: person} do
+      auth_conn = conn |> authorize(person)
+
+      response = graphql_query(auth_conn, %{query: query(), variables: space |> variables(%{name: "Random name"})}, :success)
+      assert Map.has_key?(response, "errors")
+    end
+
+
+    defp query() do
+      """
+      mutation editSpace(
+        $id: UUID4
+        $spaceInput: SpaceInput!
+      ) {
+        editSpace(
+          id: $id
+          spaceInput: $spaceInput
+        ) {
+          id
+          name
+        }
+      }
+      """
+    end
+
+    def variables(space, space_input) do
+      %{
+        id: space.id,
+        space_input: space_input
+      }
+    end
+  end
 end
